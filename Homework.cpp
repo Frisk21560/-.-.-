2c9a26f2d2649d386bfed87d531542ced9c071aa
Ілля Лень <136194383+Frisk21560@users.noreply.github.com>
2025-10-27 23:13:07 +0200

    Homework 8

--- Homework.cpp
+++ Homework.cpp
@@ -2,123 +2,59 @@
 //
 
 #include <iostream>
+#include <string>
 #include <Windows.h>
+
 using namespace std;
 
-// тут я роблю клас String
-class String {
-private:
-    char* zminna; 
-    int dovzhyna;
+// 1: шаблонна функція для виводу імені типу
+template<typename T>
+void printTypeName(const T& value) {
+    cout << "Type is: " << typeid(value).name() << endl;
+}
 
-public:
-    // конструктор по замовчуванню, створює пустий рядок
-    String() {
-        dovzhyna = 0;
-        zminna = new char[1];
-        zminna[0] = '\0';
-        // тут створюю пустий рядок
-    }
+// Перевантаження для void pointer
+void printTypeName(void* ptr) {
+    cout << "Type is: void pointer" << endl;
+}
 
-    // конструктор з параметром, приймає const char*
-    String(const char* str) {
-        // рахую довжину
-        dovzhyna = 0;
-        while (str[dovzhyna] != '\0') {
-            dovzhyna++;
-        }
-        zminna = new char[dovzhyna + 1];
-        // копіюю по одному символу
-        for (int i = 0; i < dovzhyna; i++) {
-            zminna[i] = str[i];
-        }
-        zminna[dovzhyna] = '\0';
-    }
+// 2: перетворюємо int у char через reinterpret_cast
+void intToCharReinterpret(int number) {
+    char* charPtr = reinterpret_cast<char*>(&number);
+    cout << "First byte as char (reinterpret_cast): " << *charPtr << endl;
 
-    // конструктор копіювання
-    String(const String& other) {
-        dovzhyna = other.dovzhyna;
-        zminna = new char[dovzhyna + 1];
-        for (int i = 0; i < dovzhyna; i++) {
-            zminna[i] = other.zminna[i];
-        }
-        zminna[dovzhyna] = '\0';
-        // тут просто копіюю все з іншого String
-    }
-
-    // деструктор
-    ~String() {
-        delete[] zminna;
-        // тут видаляю той масив, бо інакше буде утечка пам'яті
-    }
-
-    // метод щоб отримати довжину рядка
-    int GetDovzhyna() const {
-        return dovzhyna;
-        // повертаю довжину
-    }
-
-    // метод щоб отримати сам рядок
-    String GetString() const {
-        return *this;
-        // повертаю цей рядок як новий String
-    }
-
-    // метод щоб отримати рядок як const char*
-    const char* GetConstChar() const {
-        return zminna;
-        // повертаю просто масив символів
-    }
-
-    // оператор + для конкатенації рядків
-    String operator+(const String& right) const {
-        int newLen = dovzhyna + right.dovzhyna;
-        char* novaZminna = new char[newLen + 1];
-        // копіюю свій рядок
-        for (int i = 0; i < dovzhyna; i++) {
-            novaZminna[i] = zminna[i];
-        }
-        // копіюю другий рядок
-        for (int i = 0; i < right.dovzhyna; i++) {
-            novaZminna[dovzhyna + i] = right.zminna[i];
-        }
-        novaZminna[newLen] = '\0';
-        // тут з'єдную два рядки в один
-
-        String result(novaZminna);
-        delete[] novaZminna;
-        // створюю новий рядок, а потім видаляю тимчасовий масив
-        return result;
-    }
-};
-
-// функція щоб показати рядок
-void printString(const String& str) {
-    cout << "рядок: " << str.GetConstChar() << endl;
-    // просто виводить рядок на екран
+    // Більш очікуване перетворення значенння number - char
+    char charViaStatic = static_cast<char>(number);
+    cout << "Value converted to char (static_cast): " << charViaStatic << endl;
 }
 
 int main() {
     SetConsoleOutputCP(1251);
 
-    // створюю пару рядків
-    String z1("привіт");
-    String z2(" світ!");
+    // 1
+    int num = 42;
+    double dbl = 3.14;
+    string nameStr = "student";
 
-    printString(z1); // показую перший
-    printString(z2); // показую другий
+    cout << "Task 1 examples:" << endl;
+    printTypeName(num);
+    printTypeName(dbl);
+    printTypeName(nameStr);
 
-    // конкатеную рядки
-    String z3 = z1 + z2;
-    printString(z3); // показую результат
+    void* someptr = nullptr;
+    printTypeName(someptr);
 
-    // показую довжину
-    cout << "довжина: " << z3.GetDovzhyna() << endl;
+    cout << endl;
 
-    // отримую рядок як const char*
-    cout << "рядок як const char*: " << z3.GetConstChar() << endl;
+    // 2
+    cout << "Task 2 examples:" << endl;
+    int numberForChar = 65; // 65 -> 'A'
+    cout << "Original number = " << numberForChar << endl;
+    intToCharReinterpret(numberForChar);
 
-    // копіюю рядок
-    String z4 = z3;
-    printString(z4); // показую копію
+    int numberForChar2 = 0x00000061; // младший байт 0x61 -> 'a'
+    cout << "Another number = " << numberForChar2 << endl;
+    intToCharReinterpret(numberForChar2);
+
+    return 0;
 }
